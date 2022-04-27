6a69d87eae09e704eff347ba0ee312abc6769b35
Pawel Dudziński <pawel.dudzinski@saucelabs.com>
2022-04-27 15:33:44 +0200

    Add setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+
+setuptools.setup(
+    name="py-confluent",
+    version="0.9.1",
+    author="Pawel Dudzinski",
+    author_email="pawel.dudzinski@saucelabs.com",
+    description="Simple Python library wrapping the Confluent Cloud CLI v2",
+    long_description="Simple Python library wrapping the Confluent Cloud CLI v2",
+    long_description_content_type="text/markdown",
+    url="https://github.com/saucelabs/py-confluent",
+    py_modules=["confluent"],
+    classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "executor==23.2"
+    ]
+ )
